135ce055acb7d83923324385b0299ca0cb41ab57
aspatarelu <andrei.spatarelu@gmail.com>
2013-11-17 11:33:27 +0200

    project repository

---
+++ Wad.iFollow.Web/Wad.iFollow.Web/Controllers/WallController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Wad.iFollow.Web.Models;
+
+namespace Wad.iFollow.Web.Controllers
+{
+    public class WallController : Controller
+    {
+        //
+        // GET: /Main/
+
+        public ActionResult MainPage()
+        {
+            return View();
+        }
+
+        public ActionResult Settings()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return PartialView("_SettingsModal");
+        }
+
+        [HttpPost]
+        public ActionResult Settings(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    SaveChanges(model);
+                    return Json(new { success = true });
+                }
+                catch (Exception e)
+                {
+                    ModelState.AddModelError("", e.Message);
+                }
+
+            }
+            //Something bad happened
+            return PartialView("_SettingsModal", model);
+        }
+
+        static void SaveChanges(LoginModel model)
+        {
+            // Uncommment next line to demonstrate errors in modal
+            //throw new Exception("Error test");
+        }
+
+
+    }
+}
---
+++ Wad.iFollow.Web/Wad.iFollow.Web/Scripts/wall.js
@@ -0,0 +1,11 @@
+﻿function mainpage() { }
+function ceva() {
+    $('#a').click(function () {
+        debugger;
+        var url = "Views/Wall/_SettingsModal.cshtml";
+        $.get(url, function (data) {
+            $('#myModalContainer').html(data);
+            $('#myModal').modal('show');
+        });
+    });
+}
