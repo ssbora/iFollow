80a74844f2b2f6dd9fb8f0b257e60474f4e6210c
aspatarelu <andrei.spatarelu@gmail.com>
2013-11-17 17:15:50 +0200

    open settings modal; add search bar.

--- Wad.iFollow.Web/Wad.iFollow.Web/Scripts/wall.js
+++ Wad.iFollow.Web/Wad.iFollow.Web/Scripts/wall.js
@@ -1,11 +1,1 @@
 ﻿function mainpage() { }
-function ceva() {
-    $('#a').click(function () {
-        debugger;
-        var url = "Views/Wall/_SettingsModal.cshtml";
-        $.get(url, function (data) {
-            $('#myModalContainer').html(data);
-            $('#myModal').modal('show');
-        });
-    });
-}
